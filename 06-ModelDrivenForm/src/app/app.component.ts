26f4790694f0c3d4021fcdfd5a8acaf5d91f9107
Oguzhan Yilmaz <ogzhnylmz5@gmail.com>
2023-09-08 14:36:01 +0300

    complete v15

--- 06-ModelDrivenForm/src/app/app.component.ts
+++ 06-ModelDrivenForm/src/app/app.component.ts
@@ -52,12 +52,39 @@ export class AppComponent {
       }),
     });
 
-
     //value changes constructor icinde tanimlanir.
+    //yapilan her degisiklikte aktif olarak yakalama yapabiliyoruz.
     this.frm2.valueChanges.subscribe((data) => {
       console.log(data);
     });
+  }
 
-    //yapilan her degisiklikte aktif olarak yakalama yapabiliyoruz. 
+  markAsTouched() {
+    // this.frm2.markAsTouched();
+    this.frm2.get('name').markAsTouched({ onlySelf: true }); //formun state'i degismez ancak ilgili elemanin touch degeri degisir.
+  }
+
+  markAllAsTouched() {
+    this.frm2.get('adress').markAllAsTouched(); //formun state'i degismez ancak ilgili elemanin touch degeri degisir.
+  }
+
+  markAsUnTouch() {
+    this.frm2.markAsUntouched();
+  }
+
+  markAsDirty() {
+    this.frm2.get('name').markAsDirty(); //formun state'i degismez ancak ilgili elemanin dirty degeri degisir.
+  }
+
+  markAsPristine() {
+    this.frm2.get('name').markAsPristine(); //formun state'i degismez ancak ilgili elemanin pristine degeri degisir.
+  }
+
+  disable(){
+    this.frm2.get('name').disable();
+  }
+
+  enable(){
+    this.frm2.get('name').enable();
   }
 }
